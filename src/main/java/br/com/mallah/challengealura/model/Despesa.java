a3dcd0281a5563b9ea7d6388629042ebdf90a7fc
Antoine <antoine.elmallah@gmail.com>
2022-08-01 19:12:08 -0300

    Banco de dados da aplicação

---
+++ src/main/java/br/com/mallah/challengealura/model/Despesa.java
@@ -0,0 +1,54 @@
+package br.com.mallah.challengealura.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "despesas")
+public class Despesa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+	
+	private BigDecimal valor;
+	
+	private LocalDate data;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+}
