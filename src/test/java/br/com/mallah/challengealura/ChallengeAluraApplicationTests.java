e4ac3782a34f59380cb6ab5871bba5b95cfe2ea2
Antoine <antoine.elmallah@gmail.com>
2022-08-01 18:52:01 -0300

    Commit inicial

---
+++ src/test/java/br/com/mallah/challengealura/ChallengeAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mallah.challengealura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
