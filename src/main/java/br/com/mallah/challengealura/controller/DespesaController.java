85c7ee31e1ccf7d9c2c1f0d884e63afaadf6e833
Antoine <antoine.elmallah@gmail.com>
2022-08-02 18:18:11 -0300

    Exclusão de despesa

--- src/main/java/br/com/mallah/challengealura/controller/DespesaController.java
+++ src/main/java/br/com/mallah/challengealura/controller/DespesaController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,16 @@ public class DespesaController {
 		request.atualizar(despesa);
 		despesaRepository.save(despesa);
 		return ResponseEntity.ok("Despesa alterada.");
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> remover(@PathVariable Long id) {
+		Optional<Despesa> despesaOptional = despesaRepository.findById(id);
+		if (despesaOptional.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		despesaRepository.deleteById(id);
+		return ResponseEntity.ok("Despesa excluida");
 	}
 
 	private boolean jaExisteDespesaComMesmaDescricaoMesEAno(Despesa despesa) {
